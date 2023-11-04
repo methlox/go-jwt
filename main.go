86ca639a35fe3aa4251e6bba4dbfb331377f629a
methlox <astroyellowh@gmail.com>
2023-11-04 20:43:14 +0530

    ports and routes defined

---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	routes "github.com/methlox/go-jwt/routes"
+	"os"
+	"log"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port==""{
+		port="8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
+	router.GET("/api-1", func(c *gin.Context){
+		c.JSON(200, gin.H{"success":"Access granted for api-1"})
+	})
+
+	router.GET("/api-2", func(c *gin.Context){
+		c.JSON(200, gin.H{"success":"Access granted for api-2"})
+	})
+
+	router.Run(":" + port)
+
+}
