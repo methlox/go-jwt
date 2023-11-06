d958424c531ff0f4fd372b954538f84836f3f885
methlox <astroyellowh@gmail.com>
2023-11-06 13:18:13 +0530

    checking postman

--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -6,35 +6,35 @@ import (
 	"log"
 	"os"
 	"time"
-	"github.com/methlox/go-jwt/database"
+
 	jwt "github.com/dgrijalva/jwt-go"
+	"github.com/methlox/go-jwt/database"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-type SignedDetails struct{
-	Email 		string
-	First_name 	string
-	Last_name 	string
-	Uid 		string
-	User_type	string
-	jwt.StandardClaims 
+type SignedDetails struct {
+	Email      string
+	First_name string
+	Last_name  string
+	Uid        string
+	User_type  string
+	jwt.StandardClaims
 }
-
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 
 var SECRET_KEY string = os.Getenv("SECRET_KEY")
 
-func GenerateAllTokens(email string, firstName string, lastName string, userType string, uid string) (signedToken string, signedRefreshToken string, err error){
+func GenerateAllTokens(email string, firstName string, lastName string, userType string, uid string) (signedToken string, signedRefreshToken string, err error) {
 	claims := &SignedDetails{
-		Email : email,
+		Email:      email,
 		First_name: firstName,
-		Last_name: lastName,
-		Uid : uid,
-		User_type: userType,
+		Last_name:  lastName,
+		Uid:        uid,
+		User_type:  userType,
 		StandardClaims: jwt.StandardClaims{
 			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
 		},
@@ -46,48 +46,48 @@ func GenerateAllTokens(email string, firstName string, lastName string, userType
 		},
 	}
 
-	token ,err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
 	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
 
 	if err != nil {
 		log.Panic(err)
-		return 
+		return
 	}
 
 	return token, refreshToken, err
 }
 
-
-func ValidateToken(signedToken string) (claims *SignedDetails, msg string){
+func ValidateToken(signedToken string) (claims *SignedDetails, msg string) {
 	token, err := jwt.ParseWithClaims(
 		signedToken,
 		&SignedDetails{},
-		func(token *jwt.Token)(interface{}, error){
+		func(token *jwt.Token) (interface{}, error) {
 			return []byte(SECRET_KEY), nil
 		},
 	)
 
 	if err != nil {
-		msg=err.Error()
+		msg = err.Error()
 		return
 	}
 
-	claims, ok:= token.Claims.(*SignedDetails)
-	if !ok{
+	claims, ok := token.Claims.(*SignedDetails)
+	if !ok {
 		msg = fmt.Sprintf("the token is invalid")
 		msg = err.Error()
 		return
 	}
 
-	if claims.ExpiresAt < time.Now().Local().Unix(){
+	if claims.ExpiresAt < time.Now().Local().Unix() {
 		msg = fmt.Sprintf("token is expired")
 		msg = err.Error()
 		return
 	}
+
 	return claims, msg
 }
 
-func UpdateAllTokens(signedToken string, signedRefreshToken string, userId string){
+func UpdateAllTokens(signedToken string, signedRefreshToken string, userId string) {
 	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
 	var updateObj primitive.D
@@ -99,7 +99,7 @@ func UpdateAllTokens(signedToken string, signedRefreshToken string, userId strin
 	updateObj = append(updateObj, bson.E{"updated_at", Updated_at})
 
 	upsert := true
-	filter := bson.M{"user_id":userId}
+	filter := bson.M{"user_id": userId}
 	opt := options.UpdateOptions{
 		Upsert: &upsert,
 	}
@@ -115,7 +115,7 @@ func UpdateAllTokens(signedToken string, signedRefreshToken string, userId strin
 
 	defer cancel()
 
-	if err!=nil{
+	if err != nil {
 		log.Panic(err)
 		return
 	}
--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,31 +1,31 @@
 package middleware
 
-import(
+import (
 	"fmt"
-	"net/http"
-	helper "github.com/akhil/golang-jwt-project/helpers"
+	helper "github.com/methlox/go-jwt/helpers"
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
-func Authenticate() gin.HandlerFunc{
-	return func(c *gin.Context){
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
 		clientToken := c.Request.Header.Get("token")
-		if clientToken == ""{
-			c.JSON(http.StatusInternalServerError, gin.H{"error":fmt.Sprintf("No Authorization header provided")})
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No Authorization header provided")})
 			c.Abort()
 			return
 		}
 
 		claims, err := helper.ValidateToken(clientToken)
-		if err !="" {
-			c.JSON(http.StatusInternalServerError, gin.H{"error":err})
+		if err != "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err})
 			c.Abort()
 			return
 		}
 		c.Set("email", claims.Email)
 		c.Set("first_name", claims.First_name)
 		c.Set("last_name", claims.Last_name)
-		c.Set("uid",claims.Uid)
+		c.Set("uid", claims.Uid)
 		c.Set("user_type", claims.User_type)
 		c.Next()
 	}
